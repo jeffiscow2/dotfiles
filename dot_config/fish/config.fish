5481be1474671f6bf17917f35a7bbb5ac83156af
jeffiscow <jeffiscow2@gmail.com>
2025-04-13 01:01:20 -0400

    tmux and basic nvim setup

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,4 +5,16 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #function fish_greeting
 #    # smth smth
 #end
-set -gx EDITOR vim
+set -gx EDITOR nvim
+set -x _ZO_ECHO '1'
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+fzf --fish | source
+zoxide init fish | source
